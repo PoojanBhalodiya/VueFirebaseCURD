dda2c7c5c5933e7b4cb5bfba2e5bd88f84848bd2
Tapan Padaliya <tapan@7span.com>
2023-06-23 15:47:56 +0530

    tailwind & PWA init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template lang="">
   <div>
-    Yo
+    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
   </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
