5cf7dddf439d6de8726b7d7f275fb47dad04298c
Tapan Padaliya <tapan@7span.com>
2023-06-23 17:13:53 +0530

    pwa config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template lang="">
   <div>
     <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline text-red-400">
+      Version is <span class="text-black">{{ Version }}</span>
+    </h1>
   </div>
 </template>
 <script>
-export default {};
+import PackageJson from "../package.json";
+export default {
+  data() {
+    return {
+      Version: null,
+    };
+  },
+  created() {
+    this.Version = PackageJson.version;
+  },
+};
 </script>
 <style lang=""></style>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      base: "/",
+      srcDir: "src",
+      strategies: "generateSW",
+    }),
+    vue(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    port: 8082,
   },
 });
